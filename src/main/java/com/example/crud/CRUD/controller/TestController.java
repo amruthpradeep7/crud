48efe025c209014d90577415347cf323e39cedc6
Pradeep <amruthpradeep7@gmail.com>
2024-10-23 19:51:10 +0530

    adding test controller

---
+++ src/main/java/com/example/crud/CRUD/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.example.crud.CRUD.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    //create a method/api to calculate gst
+}
